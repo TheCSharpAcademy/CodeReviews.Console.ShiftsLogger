86a6e20c3c8c81f7dd018fdd03acfc83b038c477
Edward Vaudin <edward@vaudin.net>
2023-01-22 12:38:24 +0000

    Initial EF API setup with employee management

---
+++ Console.ShiftLogger.edvaudin/Console.ShiftLogger.API.edvaudin/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftLogger.API.Models;
+
+namespace ShiftLogger.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+    }
+}
---
+++ Console.ShiftLogger.edvaudin/Console.ShiftLogger.API.edvaudin/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftLogger.API.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+    public List<Shift>? Shifts { get; set; }
+}
