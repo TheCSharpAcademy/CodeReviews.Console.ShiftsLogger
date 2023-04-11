1da7f8811b7ec0c63aa7e04460e1111fafe8f737
Hugo Delautre <97909616+Gautatyr@users.noreply.github.com>
2023-04-11 12:41:03 +0200

    Add Gautatyr project

---
+++ Gautatyr.ShiftsLogger/ShiftsLoggerAPI/Models/Shift.cs
@@ -0,0 +1,8 @@
+﻿namespace ShiftsLoggerAPI.Models;
+
+public class Shift
+{
+    public int Id { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+}
---
+++ Gautatyr.ShiftsLogger/ShiftsLoggerUI/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using static ShiftsLoggerUI.InterfaceApi;
+
+namespace ShiftsLoggerUI;
+
+public static class Helpers
+{
+    public static void DisplayError(string error)
+    {
+        Console.WriteLine($"\n|--->  {error}  <---|\n");
+    }
+
+    public static bool ShiftExists(int id)
+    {
+        var exists = false;
+
+        try
+        {
+            if (GetShift(id).Result != null) exists = true;
+        }  
+        catch { }
+
+        return exists;
+    }
+}
