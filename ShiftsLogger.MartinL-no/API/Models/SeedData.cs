f349f762e595a6672c8bff4154e17a8904bbd4ed
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-20 19:32:14 +0200

    feature: add seed data

---
+++ ShiftsLogger.MartinL-no/API/Models/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new ShiftsLoggerContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<ShiftsLoggerContext>>()))
+        {
+            // Look for any Employees.
+            if (context.Employees.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Employees.AddRange(
+                new Employee
+                {
+                    Name = "John Doe"
+                },
+                new Employee
+                {
+                    Name = "Charlie Brown"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- ShiftsLogger.MartinL-no/API/Program.cs
+++ ShiftsLogger.MartinL-no/API/Program.cs
@@ -1,13 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ShiftsLoggerContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ShiftsLoggerDatabase")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//  Database seeding
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
