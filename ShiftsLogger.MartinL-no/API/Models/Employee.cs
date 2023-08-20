9d86fe8e41892e1ad8c028648b406b03d797972a
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-20 18:02:33 +0200

    feature: create Employee model

---
+++ ShiftsLogger.MartinL-no/API/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models;
+
+internal class Employee
+{
+    public int EmployeeId { get; set; }
+    public string Name { get; set; }
+    public ICollection<Shift> Shifts { get; set; }
+}
