816b317c34c91016b7f0a3d5e3e830d453e9bf0e
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-20 18:02:47 +0200

    feature: create Shift model

---
+++ ShiftsLogger.MartinL-no/API/Models/Shift.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models;
+
+internal class Shift
+{
+    public int ShiftId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public Employee Employee { get; set; }
+}
