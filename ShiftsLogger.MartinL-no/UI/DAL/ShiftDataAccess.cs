36e36e25e177b421d26b9d7c7b7889aa9c6c57c7
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-22 12:35:24 +0200

    feature[ui]: add data access layer

---
+++ ShiftsLogger.MartinL-no/UI/DAL/ShiftDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using UI.Models;
+
+namespace UI.DAL;
+
+public class ShiftDataAccess
+{
+    private readonly HttpClient _client;
+
+    public ShiftDataAccess(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<IEnumerable<Shift>> GetShifts()
+    {
+        return await _client.GetFromJsonAsync<List<Shift>>("api/Shift");
+    }
+
+    public async Task<Shift> GetShift(int id)
+    {
+        return await _client.GetFromJsonAsync<Shift>($"api/Shift/{id}");
+    }
+
+    public async Task<bool> UpdateShift(int id, Shift shift)
+    {
+        var response = await _client.PutAsJsonAsync($"api/Shift/{id}", shift);
+
+        return response.StatusCode == HttpStatusCode.NoContent;
+    }
+
+    public async Task<bool> AddShift(Shift shift)
+    {
+        var response = await _client.PostAsJsonAsync("api/Shift", shift);
+
+        return response.StatusCode == HttpStatusCode.Created;
+    }
+
+    public async Task<bool> DeleteShift(int id)
+    {
+        var response = await _client.DeleteAsync($"api/Shift/{id}");
+
+        return response.StatusCode == HttpStatusCode.NoContent;
+    }
+}
---
+++ ShiftsLogger.MartinL-no/UI/Program.cs
@@ -0,0 +1,17 @@
+﻿using UI.DAL;
+using UI.Models;
+
+namespace UI;
+
+internal class Program
+{
+    private static HttpClient sharedClient = new()
+    {
+        BaseAddress = new Uri("https://localhost:7012")
+    };
+
+    static async Task Main(string[] args)
+    {
+        var repo = new ShiftDataAccess(sharedClient);
+    }
+}
