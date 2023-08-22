dbbb08a4c39efe3628a786cdadb4009d5b0e133f
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-22 09:50:01 +0200

    feature: add repository/ controller

---
+++ ShiftsLogger.MartinL-no/API/Models/Interfaces/IShiftRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Models;
+
+public interface IShiftRepository
+{
+    public IEnumerable<Shift> GetShifts();
+
+    public Shift GetShift(int id);
+
+    public bool AddShift(Shift shift);
+
+    public bool DeleteShift(int id);
+
+    public bool UpdateShift(int id, Shift newShift);
+}
