33d18b567545026d8c8e46204d21ac176beef9f7
Marcus Eklund <marckus@gmail.com>
2023-10-03 16:52:48 +0200

    WebAPI part is done

---
+++ ShiftsLogger.Forser/ShiftsLoggerWebAPI/Models/ShiftContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShiftsLoggerWebAPI.Models
+{
+    public class ShiftContext : DbContext
+    {
+        public ShiftContext() { }
+        public ShiftContext(DbContextOptions<ShiftContext> options) : base(options) { }
+        public DbSet<ShiftModel> Shifts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=ShiftsLogger;Trusted_Connection=True;MultipleActiveResultSets=True;Encrypt=False");
+        }
+    }
+}
---
+++ ShiftsLogger.Forser/ShiftsLoggerWebAPI/Models/ShiftModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShiftsLoggerWebAPI.Models
+{
+    public class ShiftModel
+    {
+        public long Id {  get; set; }
+        public string? EmployeeName { get; set; }
+        public DateTime StartOfShift {  get; set; }
+        public DateTime EndOfShift { get; set; }
+        public TimeSpan Duration => EndOfShift - StartOfShift;
+    }
+}
