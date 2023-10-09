def1bbc360b7fd5fb2d534d7e3aba11eecd63dca
HappyReni <dnjsvlfwo@gmail.com>
2023-10-09 16:58:08 +0900

    submit for review

---
+++ ShfitsLogger.HappyReni/ShiftLoggerUI/Data/ShiftController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.DotNet.MSIdentity.Shared;
+using ShiftLoggerAPI.Models;
+using ShiftLoggerUI.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Shift = ShiftLoggerUI.Models.Shift;
+
+namespace ShiftLoggerUI.Data
+{
+    public static class ShiftController
+    {
+        public static async Task<Shift> GetShift(int id)
+        {
+            var shift = new Shift();
+            var endpoint = $"https://localhost:7040/api/Shifts/{id}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStreamAsync();
+                    shift = await JsonSerializer.DeserializeAsync<Shift>(json);
+                    return shift;
+                }
+                else throw new Exception("Error occured with API call.");
+            }
+        }
+        public static async Task<List<Shift>> GetShifts()
+        {
+            var shifts = new List<Shift>();
+            var endpoint = "https://localhost:7040/api/Shifts";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStreamAsync();
+                    shifts = await JsonSerializer.DeserializeAsync<List<Shift>>(json);
+                    return shifts;
+                }
+                else throw new Exception("Error occured with API call.");
+            }
+        }
+
+        public static async void AddShift(Shift shift)
+        {
+            var endpoint = "https://localhost:7040/api/Shifts";
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task response = client.PostAsJsonAsync(endpoint, shift);
+                response.Wait();
+                if (response.IsCompletedSuccessfully)
+                {
+                    UI.Write("Successfully added.");
+                }
+                else
+                {
+                    throw new Exception("Error occured with API call.");
+                }
+            }
+        }
+        public static async Task UpdateShift(int id,Shift shift)
+        {
+            var endpoint = $"https://localhost:7040/api/Shifts/{id}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.PutAsJsonAsync(endpoint, shift);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    UI.Write("Successfully Updated.");
+                }
+                else
+                {
+                    throw new Exception("Error occured with API call.");
+                }
+            }
+        }
+
+        public static async Task DeleteShift(int id)
+        {
+            var endpoint = $"https://localhost:7040/api/Shifts/{id}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.DeleteAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    UI.Write("Successfully deleted.");
+                }
+                else
+                {
+                    throw new Exception("Error occured with API call.");
+                }
+            }
+        }
+    }
+}
---
+++ ShfitsLogger.HappyReni/ShiftLoggerUI/Validation.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace ShiftLoggerUI
+{
+    public class Validation
+    {
+        public static DateTime CheckDateTime(string date)
+        {
+            string format = "yyyy-MM-dd HH:mm:ss";
+
+            if (DateTime.TryParseExact(date, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+            {
+                return DateTime.Parse(date);
+            }
+
+            throw new Exception("Invalid DateTime format.");
+        }
+    }
+}
