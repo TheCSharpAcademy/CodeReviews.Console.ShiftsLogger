18a5703b97c9abdf23c5ac8c9197c00d89cd0f94
simon davies <daviessimon21@gmail.com>
2023-11-15 16:39:13 +0000

    Redone headers in API

--- Speedierone.ShiftLogger/ShiftLoggerAPI/ShiftLoggerAPI/Models/ShiftTimes.cs
+++ Speedierone.ShiftLogger/ShiftLoggerAPI/ShiftLoggerAPI/Models/ShiftTimes.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 namespace ShiftLoggerAPI.Models
 {
     public class ShiftTimes
@@ -5,9 +7,9 @@ namespace ShiftLoggerAPI.Models
         public long Id { get; set; }
 
         public string? Name { get; set; }
-        public DateTime StartDatetime { get; set; }
-
-        public DateTime EndDatetime { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
 
         
     }
---
+++ Speedierone.ShiftLogger/ShiftLoggerUI/ShiftTrackerUI/ShiftTrackerUI/TableVisualisation.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleTableExt;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftTrackerUI
+{
+    internal class TableVisualisation
+    {
+        public static void ShowTable<T>(List<T> tableData, [AllowNull] string tableName) where T : class
+        {
+            Console.Clear();
+            if (tableName == null)
+                tableName = "";
+
+            Console.WriteLine("\n\n");
+
+            ConsoleTableBuilder
+                .From(tableData)
+                .WithColumn(tableName)
+                .WithFormat(ConsoleTableBuilderFormat.Alternative)
+                .ExportAndWriteLine(TableAligntment.Center);
+        }
+    }
+}
