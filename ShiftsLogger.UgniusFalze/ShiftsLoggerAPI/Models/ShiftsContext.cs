4acbdce06ad8d00559a50cd824c2c5c00c0ce588
ugnius <ugnius.vilkoncius@gmail.com>
2023-12-18 12:27:03 +0200

    Initial with model and context

---
+++ ShiftsLogger.UgniusFalze/ShiftsLoggerAPI/Models/ShiftsContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShiftsLoggerAPI.Models;
+
+public class ShiftsContext : DbContext
+{
+    public ShiftsContext(DbContextOptions<ShiftsContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Shift> Shifts { get; set; }
+}
---
+++ ShiftsLogger.UgniusFalze/ShiftsLoggerAPI/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ShiftsContext>(opt =>
+    opt.UseSqlServer("Server=(LocalDb)\\MSSQLLocalDB;Initial Catalog=Shifts;Integrated Security=SSPI;Trusted_Connection=yes"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
