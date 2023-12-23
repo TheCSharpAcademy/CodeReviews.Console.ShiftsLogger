a6707d67279d9cc5928dff92425d3701880deb6b
Michiel <michiel.vergauwen@visma.com>
2023-12-23 10:01:25 +0100

    Added try catch

--- ShiftsLoggerApp.MicVerg/ShiftsLoggerUI/ShiftsLoggerService.cs
+++ ShiftsLoggerApp.MicVerg/ShiftsLoggerUI/ShiftsLoggerService.cs
@@ -16,46 +16,69 @@ namespace ShiftsLoggerUI
         {
             using (var client = new HttpClient())
             {
-                var endpoint = new Uri("https://localhost:7009/api/ShiftModels");
-                var json = JsonConvert.SerializeObject(newShift);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                try
+                {
+                    var endpoint = new Uri("https://localhost:7009/api/ShiftModels");
+                    var json = JsonConvert.SerializeObject(newShift);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                var result = await client.PostAsync(endpoint, content);
+                    var result = await client.PostAsync(endpoint, content);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
             }
+            Console.ReadLine();
         }
 
         internal async Task DeleteShift(int idToDelete)
         {
             using (var client = new HttpClient())
             {
-                var endpoint = new Uri($"https://localhost:7009/api/ShiftModels/{idToDelete}");
-                var result = await client.DeleteAsync(endpoint);
+                try
+                {
+                    var endpoint = new Uri($"https://localhost:7009/api/ShiftModels/{idToDelete}");
+                    var result = await client.DeleteAsync(endpoint);
 
-                if (result.IsSuccessStatusCode)
-                {
-                    Console.WriteLine($"Shift with ID {idToDelete} deleted successfully.");
+                    if (result.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Shift with ID {idToDelete} deleted successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error deleting shift. Status code: {result.StatusCode}");
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    Console.WriteLine($"Error deleting shift. Status code: {result.StatusCode}");
+                    Console.WriteLine($"An error occurred: {ex.Message}");
                 }
-                Console.ReadLine();
             }
+            Console.ReadLine();
         }
 
         internal async Task GetShiftById(int idToGet)
         {
             using (var client = new HttpClient())
             {
-                var endpoint = new Uri($"https://localhost:7009/api/ShiftModels/{idToGet}");
-                var result = client.GetAsync(endpoint).Result;
-                var json = result.Content.ReadAsStringAsync().Result;
+                try
+                {
+                    var endpoint = new Uri($"https://localhost:7009/api/ShiftModels/{idToGet}");
+                    var result = await client.GetAsync(endpoint);
+                    var json = await result.Content.ReadAsStringAsync();
 
-                var deserializedGet = JsonConvert.DeserializeObject<ShiftModel>(json);
+                    var deserializedGet = JsonConvert.DeserializeObject<ShiftModel>(json);
 
-                Console.WriteLine($"Id: {deserializedGet.Id} | StartTime: {deserializedGet.StartTime} | EndTime: {deserializedGet.EndTime} | Worker name: {deserializedGet.WorkerName}");
-                Console.ReadLine();
+                    Console.WriteLine($"Id: {deserializedGet.Id} | StartTime: {deserializedGet.StartTime} | EndTime: {deserializedGet.EndTime} | Worker name: {deserializedGet.WorkerName}");
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
             }
+            Console.ReadLine();
         }
 
         internal async Task GetShifts()
@@ -64,19 +87,26 @@ namespace ShiftsLoggerUI
 
             using (var client = new HttpClient())
             {
-                var endpoint = new Uri("https://localhost:7009/api/ShiftModels");
-                var result = client.GetAsync(endpoint).Result;
-                var json = result.Content.ReadAsStringAsync().Result;
-
-                var deserializedGet = JsonConvert.DeserializeObject<List<ShiftModel>>(json);
-
-                Console.WriteLine("Shifts" + " |");
-                Console.WriteLine("---------------------");
-
-                foreach (var shift in deserializedGet)
+                try
                 {
-                    Console.WriteLine($"Id: {shift.Id} | StartTime: {shift.StartTime} | EndTime: {shift.EndTime} | Worker name: {shift.WorkerName}");
+                    var endpoint = new Uri("https://localhost:7009/api/ShiftModels");
+                    var result = await client.GetAsync(endpoint);
+                    var json = await result.Content.ReadAsStringAsync();
+
+                    var deserializedGet = JsonConvert.DeserializeObject<List<ShiftModel>>(json);
+
+                    Console.WriteLine("Shifts" + " |");
                     Console.WriteLine("---------------------");
+
+                    foreach (var shift in deserializedGet)
+                    {
+                        Console.WriteLine($"Id: {shift.Id} | StartTime: {shift.StartTime} | EndTime: {shift.EndTime} | Worker name: {shift.WorkerName}");
+                        Console.WriteLine("---------------------");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
                 }
             }
             Console.ReadLine();
@@ -88,11 +118,19 @@ namespace ShiftsLoggerUI
 
             using (var client = new HttpClient())
             {
-                var endpoint = new Uri($"https://localhost:7009/api/ShiftModels/{idToUpdate}");
-                var json = JsonConvert.SerializeObject(updatedShift);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var result = client.PutAsync(endpoint, content).Result;
+                try
+                {
+                    var endpoint = new Uri($"https://localhost:7009/api/ShiftModels/{idToUpdate}");
+                    var json = JsonConvert.SerializeObject(updatedShift);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var result = await client.PutAsync(endpoint, content);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
             }
+            Console.ReadLine();
         }
     }
 }
