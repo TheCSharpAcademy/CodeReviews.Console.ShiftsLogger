b43db1626eb3a572da35b2ca0c1c4dbe8bd533ba
doc415 <sipahioglu_serdar@yahoo.com>
2024-01-26 00:46:50 +0300

    Completed project

---
+++ ShiftsLogger.Doc415/ShiftsUI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ShiftsUI
+{
+    internal class Program
+    {
+        static private UserInterface _userInterface = new();
+        static void Main(string[] args)
+        {
+            _userInterface.MainMenu().Wait();
+        }
+    }
+}
