54926678edbb45b20e5ad6068a9fc52b9029176b
Stan Choi <sjc591@nyu.edu>
2024-02-22 10:06:06 -0500

    Setup entity framework with sql server for shift model

--- ShiftsLogger.StanimalTheMan/ShiftsLogger.StanimalTheMan/Program.cs
+++ ShiftsLogger.StanimalTheMan/ShiftsLogger.StanimalTheMan/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerWebAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ShiftContext>(options =>
+	options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
