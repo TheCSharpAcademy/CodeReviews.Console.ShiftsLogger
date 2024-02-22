6123fee2d254f5dc80f0ec783607a46dec503d57
Stan Choi <sjc591@nyu.edu>
2024-02-22 12:14:18 -0500

    Quickly test if I can get all shift data

---
+++ ShiftsLogger.StanimalTheMan/ShiftsLoggerConsoleUI/DataAccess.cs
@@ -0,0 +1,33 @@
+﻿namespace ShiftsLoggerConsoleUI
+{
+	internal static class DataAccess
+	{
+		internal static async Task GetAllShiftsAsync()
+		{
+			using (var httpClient = new HttpClient())
+			{
+				try
+				{
+					HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7204/api/shifts/");
+
+					if (response.IsSuccessStatusCode)
+					{
+						string responseBody = await response.Content.ReadAsStringAsync();
+						Console.WriteLine("Response from API:");
+						Console.WriteLine(responseBody);
+						// display via table later
+						Console.ReadLine();
+					}
+					else
+					{
+						Console.WriteLine($"Failed to fetch data. Status code: {response.StatusCode}");
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Server / API may be down or some other unexpected issue is going on: {ex.Message}");
+				}
+			}
+		}
+	}
+}
---
+++ ShiftsLogger.StanimalTheMan/ShiftsLoggerConsoleUI/UserInterface.cs
@@ -0,0 +1,38 @@
+﻿namespace ShiftsLoggerConsoleUI;
+
+using Spectre.Console;
+using static ShiftsLoggerConsoleUI.Enums;
+
+internal static class UserInterface
+{
+	internal static void ShowMainMenu()
+	{
+		var isAppRunning = true;
+		while (isAppRunning)
+		{
+			Console.Clear();
+			var option = AnsiConsole.Prompt(
+				new SelectionPrompt<MenuOptions>()
+				.Title("What would you like to do?")
+				.AddChoices(
+					MenuOptions.AddShift,
+					MenuOptions.DeleteShift,
+					MenuOptions.UpdateShift,
+					MenuOptions.ViewAllShifts,
+					MenuOptions.ViewShift,
+					MenuOptions.Quit));
+
+			switch (option)
+			{
+				case MenuOptions.ViewAllShifts:
+					Task task = DataAccess.GetAllShiftsAsync();
+					task.Wait();
+					break;
+				case MenuOptions.Quit:
+					Console.WriteLine("Goodbye");
+					isAppRunning = false;
+					break;
+			}
+		}
+	}
+}
