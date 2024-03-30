3ad24296bf570e352e70a0ac5bd2334930b92e4d
Paul W Saltzman <131216446+Paul-W-Saltzman@users.noreply.github.com>
2024-03-30 10:36:57 -0500

    Resolving Codacy Issues

--- ShiftLogger.PaulWSaltzman/ShiftLogger/Data/ShiftLoggerContext.cs
+++ ShiftLogger.PaulWSaltzman/ShiftLogger/Data/ShiftLoggerContext.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ShiftLogger.Models;
 
 namespace ShiftLogger.Data
 {
     public class ShiftLoggerContext : DbContext
     {
-        public ShiftLoggerContext (DbContextOptions<ShiftLoggerContext> options)
+        public ShiftLoggerContext(DbContextOptions<ShiftLoggerContext> options)
             : base(options)
         {
         }
