28fa4e3525db0690aa304afa1122ae2628eb0cd0
Dejmenek <dejmen14567@gmail.com>
2024-04-04 23:17:36 +0200

    Add models

---
+++ ShiftsLogger.Dejmenek/ShiftsLogger.Dejmenek.UI/Models/Shift.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShiftsLogger.Dejmenek.UI.Models;
+public class Shift
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("employeeFirstName")]
+    public string EmployeeFirstName { get; set; } = null!;
+    [JsonPropertyName("employeeLastName")]
+    public string EmployeeLastName { get; set; } = null!;
+    [JsonPropertyName("startTime")]
+    public DateTime StartTime { get; set; }
+    [JsonPropertyName("endTime")]
+    public DateTime EndTime { get; set; }
+    [JsonPropertyName("duration")]
+    public string Duration { get; set; } = null!;
+}
+
+public class ShiftReadDTO
+{
+    public string EmployeeFirstName { get; set; } = null!;
+    public string EmployeeLastName { get; set; } = null!;
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Duration { get; set; } = null!;
+}
+
+public class ShiftAddDTO
+{
+    public int EmployeeID { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Duration { get; set; } = null!;
+}
+
+public class ShiftUpdateDTO
+{
+    public DateTime EndTime { get; set; }
+    public string Duration { get; set; } = null!;
+}
