5a85a3590afb8aa9642ed4de52b7bfac1e010b3a
Dejmenek <dejmen14567@gmail.com>
2024-04-04 23:12:10 +0200

    Add models

---
+++ ShiftsLogger.Dejmenek/ShiftsLogger.Dejmenek.API/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace ShiftsLogger.Dejmenek.API.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public ICollection<Shift> Shifts { get; set; } = new List<Shift>();
+}
+
+public class EmployeeReadDTO
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public List<ShiftReadDTO> Shifts { get; set; } = new List<ShiftReadDTO>();
+}
+
+public class EmployeeUpdateDTO
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+}
+
+public class EmployeeCreateDTO
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+}
