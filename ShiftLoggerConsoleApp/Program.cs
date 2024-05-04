0923e18be9c7ee1ec202174004af0958b735a845
Jingwen Xia <amyfor7@gmail.com>
2024-05-04 23:20:00 +0800

    feat: init UI menu

---
+++ ShiftLoggerConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using Spectre.Console;
+enum MenuOptions
+{
+    AddShift,
+    DeleteShift,
+    UpdateShift,
+    ViewShift,
+    ViewAllShifts,
+    Quit
+}
+
+public static class Application
+{
+    public static void Main()
+    {
+        bool isAppRunning = true;
+        while (isAppRunning)
+        {
+            var option = AnsiConsole.Prompt(
+            new SelectionPrompt<MenuOptions>()
+            .Title("What would you like to do?")
+            .AddChoices(
+                MenuOptions.AddShift,
+                MenuOptions.DeleteShift,
+                MenuOptions.UpdateShift,
+                MenuOptions.ViewAllShifts,
+                MenuOptions.ViewShift,
+                MenuOptions.Quit));
+
+            switch (option)
+            {
+                case MenuOptions.AddShift:
+                    break;
+                case MenuOptions.DeleteShift:
+                    break;
+                case MenuOptions.UpdateShift:
+                    break;
+                case MenuOptions.ViewShift:
+                    break;
+                case MenuOptions.ViewAllShifts:
+                    break;
+                case MenuOptions.Quit:
+                    isAppRunning = false;
+                    break;
+            }
+        }
+    }
+}
