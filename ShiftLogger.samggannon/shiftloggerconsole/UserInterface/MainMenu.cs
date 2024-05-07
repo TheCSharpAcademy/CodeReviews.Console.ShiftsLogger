90cb8110f93e0bba2c50544b884a95271ede2621
Samuel <Sam.Gannon@sportssouth.biz>
2024-05-07 16:43:13 -0500

    // implemented menu options in the main ui

---
+++ ShiftLogger.samggannon/shiftloggerconsole/UserInterface/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+using shiftloggerconsole.UserInterface;
+using static shiftloggerconsole.UserInterface.Enums;
+
+
+namespace shiftloggerconsole.UserInterface;
+
+internal class MainMenu
+{
+    internal static void ShowMenu()
+    {
+        var appIsRunning = true;
+        while(appIsRunning)
+        {
+            Console.Clear();
+            var option = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuOptions>()
+                .Title("What would you like to do?")
+                .AddChoices(
+                    MenuOptions.AddShift,
+                    MenuOptions.ShowShift,
+                    MenuOptions.ShowShiftById,
+                    MenuOptions.EditShiftById,
+                    MenuOptions.DeleteShiftById,
+                    MenuOptions.Quit
+                    ));
+        }
+    }
+}
---
+++ ShiftLogger.samggannon/shiftloggerconsole/UserInterface/enums.cs
@@ -0,0 +1,14 @@
+﻿namespace shiftloggerconsole.UserInterface;
+
+public class Enums
+{
+    public enum MenuOptions
+    {
+        AddShift,
+        ShowShift,
+        ShowShiftById,
+        EditShiftById,
+        DeleteShiftById,
+        Quit
+    }
+}
