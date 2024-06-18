e946ce7e9f75b4f56e394f50de4117f634480d61
Jonas Karlsson <108735639+jklogimark@users.noreply.github.com>
2024-06-18 14:54:23 +0200

    Add ShiftContext and configure database
    A new DbContext class, ShiftContext, was created under the ShiftsLoggerApi.DataAccess namespace. This class is configured to use SQL Server as its data store. Also, Program.cs was updated to include the necessary DbContext in its services.

---
+++ ShiftsLogger.kalsson/ShiftsLoggerApi/DataAccess/ShiftContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerApi.Models;
+
+namespace ShiftsLoggerApi.DataAccess;
+
+public class ShiftContext : DbContext
+{
+    public ShiftContext(DbContextOptions<ShiftContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ShiftModel> ShiftModels { get; set; } = null!;
+}
--- ShiftsLogger.kalsson/ShiftsLoggerApi/Program.cs
+++ ShiftsLogger.kalsson/ShiftsLoggerApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerApi.DataAccess;
+
 namespace ShiftsLoggerApi;
 
 public class Program
@@ -12,6 +15,9 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        
+        builder.Services.AddDbContext<ShiftContext>(opt =>
+            opt.UseSqlServer("Default"));
 
         var app = builder.Build();
 
