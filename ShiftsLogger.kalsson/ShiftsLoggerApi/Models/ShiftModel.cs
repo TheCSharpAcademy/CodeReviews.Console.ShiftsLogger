7c5c038c0b2cac324febbd2ed504350800c32fe8
Jonas Karlsson <108735639+jklogimark@users.noreply.github.com>
2024-06-18 14:48:38 +0200

    Add EntityFrameworkCore and shift model
    EntityFrameworkCore packages were added to the ShiftsLoggerApi project to enable ORM capabilities. In addition, a new ShiftModel class was created to model shift data, and a connection string for a local database was added to appsettings.json.

---
+++ ShiftsLogger.kalsson/ShiftsLoggerApi/Models/ShiftModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftsLoggerApi.Models;
+
+public class ShiftModel
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the shift.
+    /// </summary>
+    [Key]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the start time of the shift.
+    /// </summary>
+    /// <value>
+    /// The start time of the shift.
+    /// </value>
+    [Required]
+    public DateTime StartShift { get; set; }
+
+    /// <summary>
+    /// Gets or sets the end time of the shift.
+    /// </summary>
+    /// <remarks>
+    /// This property represents the time at which the shift ended.
+    /// </remarks>
+    [Required]
+    public DateTime EndShift { get; set; }
+
+    /// <summary>
+    /// Gets the duration of the shift.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown when the end shift time is earlier than the start shift time.</exception>
+    public TimeSpan ShiftDuration
+    {
+        get
+        {
+            if (EndShift < StartShift)
+            {
+                throw new InvalidOperationException("End shift cannot be earlier than start shift.");
+            }
+
+            return EndShift - StartShift;
+        }
+    }
+
+}
