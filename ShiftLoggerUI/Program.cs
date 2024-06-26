3fe434c1d8de0b74b85461e4bb77051e17824e87
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2024-06-26 16:42:46 +0100

    Add OpenAPI/Swagger connected service to console project

--- ShiftLoggerUI/Program.cs
+++ ShiftLoggerUI/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShiftLoggerUI;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Waiting for server...");
+await Task.Delay(TimeSpan.FromSeconds(5));
+
+var httpClient = new HttpClient();
+var client = new APIClient("https://localhost:7045", httpClient);
+
+var allE = await client.GetAllEmployeesAsync();
+
+
+
+foreach (var item in allE)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();  
