07e71516b654f4ab6033080a12846ea8ca87c925
N-Endy <okafornel@gmail.com>
2024-07-22 10:57:31 +0100

    Add documentation to controllers

--- ShiftsLogger.UndercoverDev/WorkerShiftsAPI/Controllers/ShiftController.cs
+++ ShiftsLogger.UndercoverDev/WorkerShiftsAPI/Controllers/ShiftController.cs
@@ -3,144 +3,172 @@ using Microsoft.EntityFrameworkCore;
 using WorkerShiftsAPI.DTOs;
 using WorkerShiftsAPI.Models;
 
-namespace WorkerShiftsAPI.Controllers
+namespace WorkerShiftsAPI.Controllers;
+
+/// <summary>
+/// Provides a controller for managing worker shifts in the WorkerShiftsAPI application.
+/// </summary>
+[Route("api/shifts")]
+[ApiController]
+public class ShiftController : ControllerBase
 {
-    [Route("api/shifts")]
-    [ApiController]
-    public class ShiftController : ControllerBase
+    private readonly WorkerShiftContext _context;
+
+    public ShiftController(WorkerShiftContext context)
     {
-        private readonly WorkerShiftContext _context;
-
-        public ShiftController(WorkerShiftContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/shifts
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ShiftDTO>>> GetShifts()
-        {
-            return await _context.Shifts
-                .Include(s => s.Worker)
-                .Select(s => ShiftToDTO(s))
-                .ToListAsync();
-        }
-
-        // GET: api/shifts/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ShiftDTO>> GetShift(int id)
-        {
-            var shift = await _context.Shifts
-                .Include(s => s.Worker)
-                .Select(s => ShiftToDTO(s))
-                .FirstOrDefaultAsync(s => s.ShiftId == id);
-
-            if (shift == null)
-            {
-                return NotFound();
-            }
-
-            return shift;
-        }
-
-        // PUT: api/shifts/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutShift(int id, ShiftDTO shiftDTO)
-        {
-            if (id != shiftDTO.ShiftId)
-            {
-                return BadRequest();
-            }
-
-            var shift = await _context.Shifts.FindAsync(id);
-            if (shift == null)
-            {
-                return NotFound();
-            }
-
-            shift.ShiftId = shiftDTO.ShiftId;
-            shift.StartTime = shiftDTO.StartTime;
-            shift.EndTime = shiftDTO.EndTime;
-            shift.WorkerId = shiftDTO.WorkerId;
-
-            _context.Entry(shift).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ShiftExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/shifts
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<ShiftDTO>> PostShift(ShiftDTO shiftDTO)
-        {
-            var worker = await _context.Workers.FindAsync(shiftDTO.WorkerId);
-            if (worker == null)
-            {
-                return BadRequest("Invalid worker ID");
-            }
-
-            var shift = new Shift
-            {
-                StartTime = shiftDTO.StartTime,
-                EndTime = shiftDTO.EndTime,
-                WorkerId = shiftDTO.WorkerId
-            };
-            
-            _context.Shifts.Add(shift);
-            await _context.SaveChangesAsync();
-
-            shiftDTO.ShiftId = shift.ShiftId;
-            shiftDTO.WorkerName = worker.Name;
-
-            return CreatedAtAction(nameof(GetShift), new { id = shiftDTO.ShiftId }, ShiftToDTO(shift));
-        }
-
-        // DELETE: api/shifts/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteShift(int id)
-        {
-            var shift = await _context.Shifts.FindAsync(id);
-            if (shift == null)
-            {
-                return NotFound();
-            }
-
-            _context.Shifts.Remove(shift);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ShiftExists(int id)
-        {
-            return _context.Shifts.Any(e => e.ShiftId == id);
-        }
-
-        public static ShiftDTO ShiftToDTO(Shift shift) =>
-        new()
-        {
-            ShiftId = shift.ShiftId,
-            StartTime = shift.StartTime,
-            EndTime = shift.EndTime,
-            WorkerId = shift.WorkerId,
-            WorkerName = shift.Worker.Name
-        };
+        _context = context;
     }
+
+    /// <summary>
+    /// Gets a list of all shifts.
+    /// </summary>
+    /// <returns>A list of <see cref="ShiftDTO"/> objects representing the shifts.</returns>
+    // GET: api/shifts
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ShiftDTO>>> GetShifts()
+    {
+        return await _context.Shifts
+            .Include(s => s.Worker)
+            .Select(s => ShiftToDTO(s))
+            .ToListAsync();
+    }
+
+    /// <summary>
+    /// Gets a specific shift by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the shift to retrieve.</param>
+    /// <returns>A <see cref="ShiftDTO"/> object representing the requested shift, or a 404 NotFound response if the shift is not found.</returns>
+    // GET: api/shifts/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ShiftDTO>> GetShift(int id)
+    {
+        var shift = await _context.Shifts
+            .Include(s => s.Worker)
+            .Select(s => ShiftToDTO(s))
+            .FirstOrDefaultAsync(s => s.ShiftId == id);
+
+        if (shift == null)
+        {
+            return NotFound();
+        }
+
+        return shift;
+    }
+
+    /// <summary>
+    /// Updates an existing shift.
+    /// </summary>
+    /// <param name="id">The ID of the shift to update.</param>
+    /// <param name="shiftDTO">The updated shift data.</param>
+    /// <returns>A 204 NoContent response if the update is successful, or a 400 BadRequest response if the ID does not match the shift ID in the request body.</returns>
+    // PUT: api/shifts/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutShift(int id, ShiftDTO shiftDTO)
+    {
+        if (id != shiftDTO.ShiftId)
+        {
+            return BadRequest();
+        }
+
+        var shift = await _context.Shifts.FindAsync(id);
+        if (shift == null)
+        {
+            return NotFound();
+        }
+
+        shift.ShiftId = shiftDTO.ShiftId;
+        shift.StartTime = shiftDTO.StartTime;
+        shift.EndTime = shiftDTO.EndTime;
+        shift.WorkerId = shiftDTO.WorkerId;
+
+        _context.Entry(shift).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ShiftExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Creates a new shift.
+    /// </summary>
+    /// <param name="shiftDTO">The data for the new shift.</param>
+    /// <returns>A 201 Created response with the created shift data, or a 400 BadRequest response if the worker ID is invalid.</returns>
+    // POST: api/shifts
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<ShiftDTO>> PostShift(ShiftDTO shiftDTO)
+    {
+        var worker = await _context.Workers.FindAsync(shiftDTO.WorkerId);
+        if (worker == null)
+        {
+            return BadRequest("Invalid worker ID");
+        }
+
+        var shift = new Shift
+        {
+            StartTime = shiftDTO.StartTime,
+            EndTime = shiftDTO.EndTime,
+            WorkerId = shiftDTO.WorkerId
+        };
+
+        _context.Shifts.Add(shift);
+        await _context.SaveChangesAsync();
+
+        shiftDTO.ShiftId = shift.ShiftId;
+        shiftDTO.WorkerName = worker.Name;
+
+        return CreatedAtAction(nameof(GetShift), new { id = shiftDTO.ShiftId }, ShiftToDTO(shift));
+    }
+
+    /// <summary>
+    /// Deletes a shift by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the shift to delete.</param>
+    /// <returns>A 204 NoContent response if the deletion is successful, or a 404 NotFound response if the shift is not found.</returns>
+    // DELETE: api/shifts/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteShift(int id)
+    {
+        var shift = await _context.Shifts.FindAsync(id);
+        if (shift == null)
+        {
+            return NotFound();
+        }
+
+        _context.Shifts.Remove(shift);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool ShiftExists(int id)
+    {
+        return _context.Shifts.Any(e => e.ShiftId == id);
+    }
+
+    public static ShiftDTO ShiftToDTO(Shift shift) =>
+    new()
+    {
+        ShiftId = shift.ShiftId,
+        StartTime = shift.StartTime,
+        EndTime = shift.EndTime,
+        WorkerId = shift.WorkerId,
+        WorkerName = shift.Worker.Name
+    };
 }
+
--- ShiftsLogger.UndercoverDev/WorkerShiftsAPI/Controllers/WorkerController.cs
+++ ShiftsLogger.UndercoverDev/WorkerShiftsAPI/Controllers/WorkerController.cs
@@ -3,151 +3,185 @@ using Microsoft.EntityFrameworkCore;
 using WorkerShiftsAPI.DTOs;
 using WorkerShiftsAPI.Models;
 
-namespace WorkerShiftsAPI.Controllers
+namespace WorkerShiftsAPI.Controllers;
+
+[Route("api/workers")]
+[ApiController]
+/// <summary>
+/// Provides a controller for managing workers and their shifts in the WorkerShiftsAPI application.
+/// </summary>
+public class WorkerController : ControllerBase
 {
-    [Route("api/workers")]
-    [ApiController]
-    public class WorkerController : ControllerBase
+    private readonly WorkerShiftContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WorkerController"/> class.
+    /// </summary>
+    /// <param name="context">The worker shift context.</param>
+    public WorkerController(WorkerShiftContext context)
     {
-        private readonly WorkerShiftContext _context;
+        _context = context;
+    }
 
-        public WorkerController(WorkerShiftContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/workers
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<WorkerDTO>>> GetWorkers()
-        {
-            var workers = await _context.Workers.Include(w => w.Shifts).ToListAsync();
-            var workerDTOs = workers.Select(worker => new WorkerDTO
-            {
-                WorkerId = worker.WorkerId,
-                Name = worker.Name,
-                Shifts = worker.Shifts?.Select(ShiftController.ShiftToDTO).ToList() ?? []
-            }).ToList();
-
-            return workerDTOs;
-        }
-
-        // GET: api/workers/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<WorkerDTO>> GetWorker(int id)
-        {
-            var worker = await _context.Workers
-                .Include(w => w.Shifts)
-                .FirstOrDefaultAsync(w => w.WorkerId == id);
-
-            if (worker == null)
-            {
-                return NotFound();
-            }
-
-            var workerDTO = new WorkerDTO
-            {
-                WorkerId = worker.WorkerId,
-                Name = worker.Name,
-                Shifts = worker.Shifts?.Select(ShiftController.ShiftToDTO).ToList() ?? []
-            };
-
-            return workerDTO;
-        }
-
-        // PUT: api/workers/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutWorker(int id, WorkerDTO workerDTO)
-        {
-            if (id != workerDTO.WorkerId)
-            {
-                return BadRequest();
-            }
-
-            var worker = new Worker
-            {
-                WorkerId = workerDTO.WorkerId,
-                Name = workerDTO.Name,
-                Shifts = workerDTO.Shifts?.Select(shiftDTO => new Shift
-                {
-                    ShiftId = shiftDTO.ShiftId,
-                    StartTime = shiftDTO.StartTime,
-                    EndTime = shiftDTO.EndTime,
-                    WorkerId = shiftDTO.WorkerId
-                }).ToList() ?? []
-            };
-
-            _context.Entry(worker).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!WorkerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/workers
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<WorkerDTO>> PostWorker(WorkerDTO workerDTO)
-        {
-            var worker = new Worker
-            {
-                Name = workerDTO.Name,
-                Shifts = workerDTO.Shifts?.Select(shiftDTO => new Shift
-                {
-                    StartTime = shiftDTO.StartTime,
-                    EndTime = shiftDTO.EndTime,
-                    WorkerId = shiftDTO.WorkerId
-                }).ToList() ?? []
-            };
-
-            _context.Workers.Add(worker);
-            await _context.SaveChangesAsync();
-
-            workerDTO.WorkerId = worker.WorkerId;
-
-            return CreatedAtAction(nameof(GetWorker), new { id = workerDTO.WorkerId }, WorkerToDTO(worker));
-        }
-
-        // DELETE: api/workers/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteWorker(int id)
-        {
-            var worker = await _context.Workers.FindAsync(id);
-            if (worker == null)
-            {
-                return NotFound();
-            }
-
-            _context.Workers.Remove(worker);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool WorkerExists(int id)
-        {
-            return _context.Workers.Any(e => e.WorkerId == id);
-        }
-
-        private static WorkerDTO WorkerToDTO(Worker worker) =>
-        new()
+    /// <summary>
+    /// Gets a list of all workers and their associated shifts.
+    /// </summary>
+    /// <returns>A list of <see cref="WorkerDTO"/> objects representing the workers and their shifts.</returns>
+    // GET: api/workers
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<WorkerDTO>>> GetWorkers()
+    {
+        var workers = await _context.Workers.Include(w => w.Shifts).ToListAsync();
+        var workerDTOs = workers.Select(worker => new WorkerDTO
         {
             WorkerId = worker.WorkerId,
-            Name = worker.Name
-        };
+            Name = worker.Name,
+            Shifts = worker.Shifts?.Select(ShiftController.ShiftToDTO).ToList() ?? []
+        }).ToList();
+
+        return workerDTOs;
     }
+
+    /// <summary>
+    /// Gets a specific worker and their associated shifts.
+    /// </summary>
+    /// <param name="id">The ID of the worker to retrieve.</param>
+    /// <returns>A <see cref="WorkerDTO"/> object representing the worker and their shifts.</returns>
+    // GET: api/workers/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<WorkerDTO>> GetWorker(int id)
+    {
+        var worker = await _context.Workers
+            .Include(w => w.Shifts)
+            .FirstOrDefaultAsync(w => w.WorkerId == id);
+
+        if (worker == null)
+        {
+            return NotFound();
+        }
+
+        var workerDTO = new WorkerDTO
+        {
+            WorkerId = worker.WorkerId,
+            Name = worker.Name,
+            Shifts = worker.Shifts?.Select(ShiftController.ShiftToDTO).ToList() ?? []
+        };
+
+        return workerDTO;
+    }
+
+    /// <summary>
+    /// Updates an existing worker and their associated shifts.
+    /// </summary>
+    /// <param name="id">The ID of the worker to update.</param>
+    /// <param name="workerDTO">The updated worker information.</param>
+    /// <returns>A no-content response if the update is successful.</returns>
+    // PUT: api/workers/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutWorker(int id, WorkerDTO workerDTO)
+    {
+        if (id != workerDTO.WorkerId)
+        {
+            return BadRequest();
+        }
+
+        var worker = await _context.Workers.FindAsync(id);
+        if (worker == null)
+        {
+            return NotFound();
+        }
+
+        worker.Name = workerDTO.Name;
+        worker.Shifts = workerDTO.Shifts?.Select(shiftDTO => new Shift
+        {
+            StartTime = shiftDTO.StartTime,
+            EndTime = shiftDTO.EndTime,
+            WorkerId = shiftDTO.WorkerId
+        }).ToList() ?? [];
+
+        _context.Entry(worker).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!WorkerExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Creates a new worker and their associated shifts.
+    /// </summary>
+    /// <param name="workerDTO">The new worker information.</param>
+    /// <returns>The created <see cref="WorkerDTO"/> object.</returns>
+    // POST: api/workers
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<WorkerDTO>> PostWorker(WorkerDTO workerDTO)
+    {
+        var worker = new Worker
+        {
+            Name = workerDTO.Name,
+            Shifts = workerDTO.Shifts?.Select(shiftDTO => new Shift
+            {
+                StartTime = shiftDTO.StartTime,
+                EndTime = shiftDTO.EndTime,
+                WorkerId = shiftDTO.WorkerId
+            }).ToList() ?? []
+        };
+
+        _context.Workers.Add(worker);
+        await _context.SaveChangesAsync();
+
+        workerDTO.WorkerId = worker.WorkerId;
+
+        return CreatedAtAction(nameof(GetWorker), new { id = workerDTO.WorkerId }, WorkerToDTO(worker));
+    }
+
+    /// <summary>
+    /// Deletes a worker and their associated shifts.
+    /// </summary>
+    /// <param name="id">The ID of the worker to delete.</param>
+    /// <returns>A no-content response if the deletion is successful.</returns>
+    // DELETE: api/workers/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteWorker(int id)
+    {
+        var worker = await _context.Workers.FindAsync(id);
+        if (worker == null)
+        {
+            return NotFound();
+        }
+
+        _context.Workers.Remove(worker);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool WorkerExists(int id)
+    {
+        return _context.Workers.Any(e => e.WorkerId == id);
+    }
+
+    private static WorkerDTO WorkerToDTO(Worker worker) =>
+    new()
+    {
+        WorkerId = worker.WorkerId,
+        Name = worker.Name,
+        Shifts = worker.Shifts?.Select(ShiftController.ShiftToDTO).ToList() ?? []
+    };
 }
+
