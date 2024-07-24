b42b8e99c35d98c65b840ff3a7168984ff67daef
N-Endy <okafornel@gmail.com>
2024-07-24 21:32:26 +0100

    Delete worker

--- ShiftsLogger.UndercoverDev/WorkerShiftsUI/Views/WorkersView.cs
+++ ShiftsLogger.UndercoverDev/WorkerShiftsUI/Views/WorkersView.cs
@@ -2,6 +2,7 @@
 using Spectre.Console;
 using WorkerShiftsUI.Models;
 using WorkerShiftsUI.Services;
+using WorkerShiftsUI.UserInteractions;
 
 namespace WorkerShiftsUI.Views;
 public class WorkersView : IWorkersView
@@ -15,33 +16,40 @@ public class WorkersView : IWorkersView
 
     public async Task WorkersMenu()
     {
-        var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
-            .Title("Shift Logger Workers Menu")
-            .AddChoices(
-                "View Workers",
-                "Add Worker",
-                "Update Worker",
-                "Delete Worker",
-                "Go Back")
-            .UseConverter(x => x.ToString())
-        );
+        var isWorkerViewRunning = true;
 
-        switch (choice)
+        while (isWorkerViewRunning)
         {
-            case "View Workers":
-                await _workerService!.ViewWorkers();
-                break;
-            // case "Add Worker":
-            //     await _workerService!.AddWorker(1);
-            //     break;
-            // case "Update Worker":
-            //     await _workerService!.UpdateWorker();
-            //     break;
-            // case "Delete Worker":
-            //     await _workerService!.DeleteWorker(1);
-                break;
-            case "Go Back":
-                return;
+            var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                .Title("Shift Logger Workers Menu")
+                .AddChoices(
+                    "View Workers",
+                    "Add Worker",
+                    "Update Worker",
+                    "Delete Worker",
+                    "Go Back")
+                .UseConverter(x => x.ToString())
+            );
+
+            switch (choice)
+            {
+                case "View Workers":
+                    await _workerService!.ViewWorkers();
+                    break;
+                case "Add Worker":
+                    Worker worker = UserInteraction.GetWorkerDetails();
+                    await _workerService!.AddWorker(worker);
+                    break;
+                // case "Update Worker":
+                //     await _workerService!.UpdateWorker();
+                //     break;
+                case "Delete Worker":
+                    await _workerService!.DeleteWorker();
+                    break;
+                case "Go Back":
+                    isWorkerViewRunning = false;
+                    return;
+            }
         }
     }
 
