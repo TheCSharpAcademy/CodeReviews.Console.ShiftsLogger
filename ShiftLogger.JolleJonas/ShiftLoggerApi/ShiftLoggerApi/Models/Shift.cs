379bdf981d3634eba2fe85529bd79f5bf67a1ea9
jollejonas <jollevb@hotmail.com>
2024-09-10 09:46:55 +0200

    Project finished. (#56)
    * Added API project.
    
    * Created ShiftController.
    Changed the controller so only 1 shift can be opened at a time and only able to change open shift.
    
    * Set up console applikation to make API calls.
    
    * Updated API calls to use try-catch blocks.

---
+++ ShiftLogger.JolleJonas/ShiftLoggerApi/ShiftLoggerApi/Models/Shift.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftLoggerApi.Models;
+
+public class Shift
+{
+    public int Id { get; set; }
+    public string? EmployeeName { get; set; }
+    [Required]
+    public DateTime Start { get; set; }
+    public DateTime? End { get; set; }
+    public TimeSpan? Duration { get; set; }
+}
---
+++ ShiftLogger.JolleJonas/ShiftLoggerApi/ShiftLoggerUI/Models/MenuOption.cs
@@ -0,0 +1,13 @@
+﻿namespace ShiftLoggerUI.Models;
+
+public class MenuOption
+{
+    public string Name { get; set; }
+    public Func<Task> Action { get; set; }
+
+    public MenuOption(string name, Func<Task> action)
+    {
+        Name = name;
+        Action = action;
+    }
+}
