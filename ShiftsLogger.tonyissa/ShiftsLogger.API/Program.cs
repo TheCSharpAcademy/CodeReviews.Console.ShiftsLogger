b0fcb28433b36d5fa7b6679a472b7f012167fda7
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-09-19 17:31:10 -0400

    Configure project to use Swagger and XML documentation

--- ShiftsLogger.tonyissa/ShiftsLogger.API/Program.cs
+++ ShiftsLogger.tonyissa/ShiftsLogger.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Shifts Logger API",
+        Version = "v1",
+        Description = "API for logging work shifts"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    s.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
