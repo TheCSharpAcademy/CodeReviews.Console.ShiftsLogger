1ced5dd57532b21fc8441451c78141887f746efb
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-09-21 18:09:27 -0400

    Add Shift entry model to project

---
+++ ShiftsLogger.tonyissa/ShiftsLogger.Client/Models/ShiftsEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace ShiftsLogger.Client.Models;
+
+public class ShiftsEntry
+{
+    public long Id { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+}
