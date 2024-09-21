faba5d15669bc1baf8bb10542c0ba78eae878320
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-09-21 19:40:55 -0400

    Add methods to repository to consume REST API

---
+++ ShiftsLogger.tonyissa/ShiftsLogger.Client/Repositories/ShiftsRepository.cs
@@ -0,0 +1,46 @@
+﻿using ShiftsLogger.Client.Models;
+using ShiftsLogger.UI.Utils;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace ShiftsLogger.Client.Repositories;
+
+public static class ShiftsRepository
+{
+    public static async Task<List<ShiftsEntry>> GetShiftsAsync()
+    {
+        using var client = new HttpClient();
+        var url = ApiRoutes.GetShiftsUrl();
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStreamAsync();
+        var shiftsList = await JsonSerializer.DeserializeAsync<List<ShiftsEntry>>(json);
+
+        return shiftsList ?? [];
+    }
+
+    public static async Task CreateShiftAsync(ShiftsEntry entry)
+    {
+        using var client = new HttpClient();
+        var url = ApiRoutes.CreateShiftUrl();
+        var response = await client.PostAsJsonAsync(url, entry);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public static async Task UpdateShiftAsync(ShiftsEntry entry)
+    {
+        using var client = new HttpClient();
+        var url = ApiRoutes.UpdateShiftUrl(entry.Id);
+        var response = await client.PostAsJsonAsync(url, entry);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public static async Task DeleteShiftAsync(long id)
+    {
+        using var client = new HttpClient();
+        var url = ApiRoutes.DeleteShiftUrl(id);
+        var response = await client.DeleteAsync(url);
+        response.EnsureSuccessStatusCode();
+    }
+}
