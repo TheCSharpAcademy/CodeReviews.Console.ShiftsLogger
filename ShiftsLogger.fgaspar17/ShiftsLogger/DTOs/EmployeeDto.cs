21f1ec3ad14b343423b737779b9b8ff021db04e4
Francisco Gaspar Serón <130596849+fgaspar17@users.noreply.github.com>
2024-10-06 19:56:10 +0200

    project finished (#60)
    * project finished
    
    * unused usings
    
    * README line length
    
    * connection API error

---
+++ ShiftsLogger.fgaspar17/ShiftsLogger/DTOs/EmployeeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ShiftsLogger;
+
+public class EmployeeDto
+{
+    public int EmployeeId { get; set; }
+    public required string Name { get; set; }
+    public List<ShiftDto> Shifts { get; set; }
+}
---
+++ ShiftsLogger.fgaspar17/ShiftsLoggerLibrary/Mappers/ShiftMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace ShiftsLoggerLibrary;
+
+public class ShiftMapper
+{
+    public static ShiftDto MapToDto(Shift shift)
+    {
+        return new ShiftDto
+        {
+            Id = shift.ShiftId,
+            Start = shift.Start,
+            End = shift.End,
+            DurationMinutes = shift.DurationMinutes,
+            EmployeeName = shift.EmployeeName,
+        };
+    }
+}
