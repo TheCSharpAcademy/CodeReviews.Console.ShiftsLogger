ff8b0eb1050d984777862fd7407b3f6de4aacb4e
Depeesh Gurung <158469668+lawangGurung@users.noreply.github.com>
2024-10-07 22:25:50 +0530

    Shifts Logger completed (#63)
    * Shifts Logger completed
    
    * Added Choice to update the properties of shift
    
    * Console app added for existing project file in webAPI
    
    ---------
    
    Co-authored-by: Depeesh Gurung <depeshgurung44@gmail.com>

---
+++ ShiftLogger.Lawang/ShiftLogger.Console_App/Program.cs
@@ -0,0 +1,8 @@
+﻿using ShiftLogger.Console_App;
+
+var userInput = new UserInput();
+
+string url = "http://localhost:5008/api/shifts";
+var shiftService = new ShiftService(url);
+var app = new Application(userInput, shiftService);
+await app.Build();
