562a9440c47f460d63db04fbb506b2264b6f0e76
Aditya Vaidya <adityavaidya83@gmail.com>
2024-11-09 00:52:37 -0500

    fixing codacy issues

--- ShiftsLogger.AdityaVaidya/ShiftLogger.ShiftTrack/App.cs
+++ ShiftsLogger.AdityaVaidya/ShiftLogger.ShiftTrack/App.cs
@@ -1,6 +1,4 @@
 ﻿using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Text;
 using ShiftLogger.ShiftTrack.Views;
 using ShiftLogger.ShiftTrack.Models;
 using ShiftLogger.ShiftTrack.Services;
