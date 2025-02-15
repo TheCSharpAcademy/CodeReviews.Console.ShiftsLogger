e4e899374a969aa096f2921caf3d8aea1d1bd46a
FunRunRushFlush <funrunrush@gmx.de>
2025-02-16 00:08:54 +0100

    final Refactor

--- ShiftsLogger.FunRunRushFlush/ShiftsLogger.Backend/ShiftsLogger.App/Services/Interfaces/IUserInputValidationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ShiftsLogger.Domain;
-
-namespace ShiftsLogger.App.Services.Interfaces
-{
-    public interface IUserInputValidationService
-    {
-        Shift ValidateUserInput(Shift? existingEntry = null);
-    }
-}
