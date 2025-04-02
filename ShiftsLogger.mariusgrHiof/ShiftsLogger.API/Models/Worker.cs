048e4fb324a471820e3dcf22b140e1d6c6f26d15
weakiepedia <wiktorczekirda@outlook.com>
2025-04-02 10:18:50 +0200

    Delete ShiftsLogger.mariusgrHiof directory

--- ShiftsLogger.mariusgrHiof/ShiftsLogger.API/Models/Worker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShiftsLogger.API.Models;
-public class Worker
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public ICollection<Shift> Shifts { get; set; }
-}
-
