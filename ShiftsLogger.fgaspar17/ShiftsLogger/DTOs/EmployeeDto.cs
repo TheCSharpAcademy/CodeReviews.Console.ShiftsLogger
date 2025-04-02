fcac3a6dc5995935498524d4e2f9fea7d36e30fd
weakiepedia <wiktorczekirda@outlook.com>
2025-04-02 10:18:19 +0200

    Delete ShiftsLogger.fgaspar17 directory

--- ShiftsLogger.fgaspar17/ShiftsLogger/DTOs/EmployeeDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShiftsLogger;
-
-public class EmployeeDto
-{
-    public int EmployeeId { get; set; }
-    public required string Name { get; set; }
-    public List<ShiftDto> Shifts { get; set; }
-}
