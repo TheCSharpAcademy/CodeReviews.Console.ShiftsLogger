5e8501db9bd1120e9d413c6457e3bf309f22433f
weakiepedia <wiktorczekirda@outlook.com>
2025-04-02 10:18:44 +0200

    Delete ShiftsLogger.kwm0304 directory

--- ShiftsLogger.kwm0304/Shared/Dtos/EmployeeShiftDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.Models.Dtos;
-
-public class EmployeeShiftDto
-{
-  public int EmployeeId { get; set; }
-  public int ShiftId { get; set; }
-  public DateTime ClockInTime { get; set; }
-  public DateTime ClockOutTime { get; set; }
-}
