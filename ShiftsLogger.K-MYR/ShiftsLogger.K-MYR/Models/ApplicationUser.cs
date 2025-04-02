0b186bbecae4711e4835cd5597d0eca432db8019
weakiepedia <wiktorczekirda@outlook.com>
2025-04-02 10:17:05 +0200

    Delete ShiftsLogger.K-MYR directory

--- ShiftsLogger.K-MYR/ShiftsLogger.K-MYR/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace ShiftsLogger.K_MYR;
-
-public class ApplicationUser : IdentityUser
-{
-    public ICollection<Shift> Shifts { get; set; } = new List<Shift>();
-}
