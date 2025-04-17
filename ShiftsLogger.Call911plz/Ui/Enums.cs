9f677a3e012eaa11bd45545450ecdf79d36d948b
Hieu Truong <htruong6219@gmail.com>
2025-04-16 19:30:26 -0700

    feat: add menu display

--- ShiftsLogger.Call911plz/Ui/Enums.cs
+++ ShiftsLogger.Call911plz/Ui/Enums.cs
@@ -1,6 +1,7 @@
 
 public static class MenuEnums
 {
-    public enum Main { MANAGESHIFT }
+    public enum Main { MANAGESHIFT, MANAGEWORKER, EXIT }
     public enum Shift { CREATESHIFT, READSHIFT, UPDATESHIFT, DELETESHIFT, BACK }
+    public enum Worker { CREATEWORKER, READWORKER, UPDATEWORKER, DELETEWORKER, BACK }
 }
