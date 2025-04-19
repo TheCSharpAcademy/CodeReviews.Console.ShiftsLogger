d6f8ef641f8e849d29c16c5e8cbc3c2bdbf8b250
Hieu Truong <htruong6219@gmail.com>
2025-04-19 15:25:54 -0700

    refactor: WorkerService to new file

--- ShiftsLogger.Call911plz/Ui/Models/WorkerService.cs
+++ ShiftsLogger.Call911plz/Ui/Models/WorkerService.cs
@@ -1,37 +1,5 @@
 using RestSharp;
-using Spectre.Console;
 using System.Text.Json;
-using System.Threading.Tasks;
-using System.Net;
-
-public class IApiService()
-{
-    internal RestClient _client;
-    internal JsonSerializerOptions _jsonOptions = new()
-    {
-        PropertyNameCaseInsensitive = true
-    };
-
-    public void ConnectApi()
-    {
-        _client = new(
-            new RestClientOptions("http://localhost:5295/api/")
-        );
-    }
-
-    internal async Task<RestResponse?> ExecuteRestAsync(Func<Task<RestResponse>> requestFunc)
-    {
-        RestResponse response = await requestFunc();
-
-        if (Errors.Codes.TryGetValue(response.StatusCode, out string? value))
-        {
-            AnsiConsole.MarkupLine(value);
-            return null;
-        }
-
-        return response;
-    }
-}
 
 public class WorkerService() : IApiService()
 {
