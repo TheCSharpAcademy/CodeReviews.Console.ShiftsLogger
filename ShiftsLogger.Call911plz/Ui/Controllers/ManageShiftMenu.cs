fb72bdc9cf771a074bbee30b83603419c5aab942
Hieu Truong <htruong6219@gmail.com>
2025-04-20 15:36:24 -0700

    feat: add base class for menus

--- ShiftsLogger.Call911plz/Ui/Controllers/ManageShiftMenu.cs
+++ ShiftsLogger.Call911plz/Ui/Controllers/ManageShiftMenu.cs
@@ -1,4 +1,5 @@
 
+
 public class ManageShiftMenuController : MenuControllerBase
 {
     internal override async Task<bool> HandleMenuSelectionAsync()
@@ -7,16 +8,40 @@ public class ManageShiftMenuController : MenuControllerBase
         switch (input)
         {
             case MenuEnums.Shift.CREATESHIFT:
+                await CreateShiftAsync();
                 break;
             case MenuEnums.Shift.READSHIFT:
+                await ReadShiftAsync();
                 break;
             case MenuEnums.Shift.UPDATESHIFT:
+                await UpdateShiftAsync();
                 break;
             case MenuEnums.Shift.DELETESHIFT:
+                await DeleteShiftAsync();
                 break;
             case MenuEnums.Shift.BACK:
                 return true;
         }
         return false;
+    }
+
+    private async Task CreateShiftAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task ReadShiftAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task UpdateShiftAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task DeleteShiftAsync()
+    {
+        throw new NotImplementedException();
     }
 }
--- ShiftsLogger.Call911plz/Ui/Controllers/ManageWorkerMenu.cs
+++ ShiftsLogger.Call911plz/Ui/Controllers/ManageWorkerMenu.cs
@@ -1,4 +1,5 @@
 
+
 public class ManageWorkerMenuController : MenuControllerBase
 {
     internal override async Task<bool> HandleMenuSelectionAsync()
@@ -7,18 +8,48 @@ public class ManageWorkerMenuController : MenuControllerBase
         switch (input)
         {
             case MenuEnums.Worker.CREATEWORKER:
+                await CreateWorkerAsync();
                 break;
             case MenuEnums.Worker.READWORKER:
+                await ReadWorkerAsync();
                 break;
             case MenuEnums.Worker.READWORKERBYID:
+                await ReadWorkerByIdAsync();
                 break;
             case MenuEnums.Worker.UPDATEWORKER:
+                await UpdateWorkerAsync();
                 break;
             case MenuEnums.Worker.DELETEWORKER:
+                await DeleteWorkerAsync();
                 break;
             case MenuEnums.Worker.BACK:
                 return true;
         }
         return false;
+    }
+
+    private async Task CreateWorkerAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task ReadWorkerAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task ReadWorkerByIdAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task UpdateWorkerAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task DeleteWorkerAsync()
+    {
+        throw new NotImplementedException();
     }
 }
