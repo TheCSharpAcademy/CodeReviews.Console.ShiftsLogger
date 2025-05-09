037c70b4e91854fd9779d1dff1b9e11e25eb7cfb
RyanW84 <ryanweavers@gmail.com>
2025-05-09 14:12:01 +0100

    starting again

--- ShiftsLogger.Ryanw84/FrontEnd/Menus/MainMenu.cs
+++
@@ -1,7 +0,0 @@
-﻿using FrontEnd.Services;
-using ShiftsLogger.Ryanw84.Services;
-using Spectre.Console;
-
-namespace FrontEnd.Menus;
-
-
--- ShiftsLogger.Ryanw84/ShiftsLogger.Ryanw84/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShiftsLogger.Ryanw84.Models;
-
-public class Location
-{
-    public int LocationId { get; set; }
-    public string? Name { get; set; }
-
-    public virtual ICollection<Shift>? Shifts { get; set; }
-}
