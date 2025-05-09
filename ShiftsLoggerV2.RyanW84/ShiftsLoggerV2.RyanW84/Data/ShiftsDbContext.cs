d834d71e1417dd43d8e3f2d3524d4ed05e9f753d
RyanW84 <ryanweavers@gmail.com>
2025-05-09 14:28:23 +0100

    setting up v2 with DbContext and Connectiong Strings

---
+++ ShiftsLoggerV2.RyanW84/ShiftsLoggerV2.RyanW84/Data/ShiftsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShiftsLoggerV2.RyanW84.Data;
+
+public class ShiftsDbContext : DbContext
+{
+    public ShiftsDbContext(DbContextOptions options)
+        : base(options) { }
+}
---
+++ ShiftsLoggerV2.RyanW84/ShiftsLoggerV2.RyanW84/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+
+using Scalar.AspNetCore;
+
+using ShiftsLoggerV2.RyanW84.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+	{
+
+	app.MapOpenApi();
+	app.MapScalarApiReference(options =>
+	{
+		options
+			.WithTitle("Shifts Logger API")
+			.WithTheme(ScalarTheme.Saturn)
+			.WithDefaultHttpClient(ScalarTarget.CSharp , ScalarClient.HttpClient)
+			.WithModels(true)
+			.WithLayout(ScalarLayout.Modern);
+	});
+
+	}
+
+app.MapControllers();
+
+
+app.Run();
