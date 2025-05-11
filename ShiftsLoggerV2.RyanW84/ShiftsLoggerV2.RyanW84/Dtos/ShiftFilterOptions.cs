372ed8d04f10e756936d2af93fb18750143f33a6
Ryanw84 <Ryan.weavers@gmail.com>
2025-05-11 21:25:34 +0100

    extended the queries to allow for the start and end date to actually appear in scalar

--- ShiftsLoggerV2.RyanW84/ShiftsLoggerV2.RyanW84/Dtos/ShiftFilterOptions.cs
+++ ShiftsLoggerV2.RyanW84/ShiftsLoggerV2.RyanW84/Dtos/ShiftFilterOptions.cs
@@ -4,15 +4,21 @@ namespace ShiftsLoggerV2.RyanW84.Dtos;
 
 public class ShiftFilterOptions
 {
-    [FromQuery(Name = "startTime")]
-    public DateTimeOffset? StartTime { get; set; }
+	[FromQuery(Name = "startTime")]
+	public DateTimeOffset? StartTime { get; set; }
 
-    [FromQuery(Name = "endTime")]
-    public DateTimeOffset? EndTime { get; set; }
+	[FromQuery(Name = "endTime")]
+	public DateTimeOffset? EndTime { get; set; }
 
-    [FromQuery(Name = "workerId")]
-    public int? WorkerId { get; set; }
+	[FromQuery(Name = "startDate")]
+	public DateTime? StartDate { get; set; } // Use DateTime for date-only filtering
 
-    [FromQuery(Name = "locationId")]
-    public int? LocationId { get; set; }
+	[FromQuery(Name = "endDate")]
+	public DateTime? EndDate { get; set; } // Use DateTime for date-only filtering
+
+	[FromQuery(Name = "workerId")]
+	public int? WorkerId { get; set; }
+
+	[FromQuery(Name = "locationId")]
+	public int? LocationId { get; set; }
 }
